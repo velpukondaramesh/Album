e9430de88e5eb6986332147434d2db9214656847
rameshvelpukonda <velpukondaramesh@gmail.com>
2017-10-15 10:05:48 +0530

    Added AlbumRest module for rest api using zend framework 2

--- config/application.config.php
+++ config/application.config.php
@@ -2,10 +2,11 @@
 return array(
     'modules' => array(
         'Application',
+        'AlbumRest',
         'Album',
     ),
     'module_listener_options' => array(
-        'config_glob_paths'    => array(
+        'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
         ),
         'module_paths' => array(
--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,9 +14,9 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn'            => 'mysql:dbname=zf2tutorial;hostname=localhost',
-        'username'       => 'rob',
-        'password'       => '123456',
+        'dsn'            => 'mysql:dbname=album;hostname=localhost',
+        'username'       => 'root',
+        'password'       => '',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
